839a41907554086236a901f132ffec727bd61e12
Rafael Simplicio <simplicio.rsb@gmail.com>
2023-09-09 21:43:07 -0300

    feat: salvando usuarios em um arquivo fisico

--- src/server.js
+++ src/server.js
@@ -1,7 +1,10 @@
 import http from 'node:http'
-import { json } from './middlewares/json.js'
+import { randomUUID } from 'node:crypto'
 
-const users = []
+import { json } from './middlewares/json.js'
+import { Database } from './database.js'
+
+const database = new Database()
 
 const server = http.createServer(async (req, res) => {
   const { method, url } = req
@@ -9,16 +12,22 @@ const server = http.createServer(async (req, res) => {
   await json(req, res)
 
   if (method === 'GET' && url === '/users') {
+    const users = database.select('users')
+
     return res.end(JSON.stringify(users))
   }
 
   if (method === 'POST' && url === '/users') {
     const { name, email } = req.body
-    users.push({
-      id: 1,
+
+    const user = {
+      id: randomUUID(),
       name,
       email,
-    })
+    }
+
+    database.insert('users', user)
+
     return res.writeHead(201).end()
   }
 
