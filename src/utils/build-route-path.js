20b094eb30ff5330cbeb609df37aa71a9e63512e
Rafael Simplicio <simplicio.rsb@gmail.com>
2023-09-09 23:04:54 -0300

    feat: rota para deletar um usuario

--- src/utils/build-route-path.js
+++ src/utils/build-route-path.js
@@ -1,9 +1,9 @@
 // /users/:id
 export function buildRoutePath(path) {
   const routeParametersRegex = /:([a-zA-Z]+)/g
-  const pathWithParams = path.replaceAll(routeParametersRegex, '([a-z0-9-_]+)')
+  const paramsWithParams = path.replaceAll(routeParametersRegex, '(?<$1>[a-z0-9\-_]+)')
 
-  const pathRegex = new RegExp(`ˆ${pathWithParams}`)
+  const pathRegex = new RegExp(`ˆ${paramsWithParams}`)
 
   return pathRegex
 }
